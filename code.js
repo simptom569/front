1db9b623bc1304491581455f5218a962ae8da93e
simptom569 <60626247+simptom569@users.noreply.github.com>
2023-10-22 00:27:53 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 var WebApp = window.Telegram.WebApp;
 const searchParams = new URLSearchParams(window.location.search);
-WebApp.showAlert("hello");
+var x = new XMLHttpRequest();
+let train = searchParams.get('train');
+let van = searchParams.get('van');
+x.open("GET", `https://82fe-87-117-49-169.ngrok-free.app/?train=$(train)&van=$(van)`, true);
+x.onload = function (){
+    WebApp.showAlert(x.responseText);
+}
+x.send(null);
